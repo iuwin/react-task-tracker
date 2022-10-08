197470ee150ced02df503c497262221a925a733f
Erwin Amper <erwinamper27@gmail.com>
2022-10-08 20:53:31 +0800

    tracker ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from  './components/Header'
+import Tasks from './components/Tasks'
+import './index.css'
 
 function App() {
+
+  const [tasks, setTasks] = useState(
+    [
+      {
+        "text": "Сходить на секонд хенд",
+        "day": "Jan 29, 2023",
+        "reminder": true,
+        "id": 5
+      },
+      {
+        "text": "Купить Игорю трусы и футболки",
+        "day": "Oct 12, 2022",
+        "reminder": false,
+        "id": 6
+      },
+      {
+        "text": "Купить даше сумку с гарри поттером розовую",
+        "day": "Mar 25, 2023",
+        "reminder": false,
+        "id": 7
+      },
+      {
+        "text": "Купить колоду затмение амири",
+        "day": "Oct 20, 2016",
+        "reminder": false,
+        "id": 8
+      },
+      {
+        "text": "Больше звезд которие падают",
+        "day": "Sep 26, 2013",
+        "reminder": false,
+        "id": 9
+      },
+      {
+        "text": "Метеорити которие падают",
+        "day": "Dec 28, 2017",
+        "reminder": false,
+        "id": 10
+      }
+    ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-wrapper'>
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Button from './Button';
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <h1>Tasks Tracker</h1>
+        <Button color="blue" text="Add" />
+    </header>
+  )
+}
+
+export default Header
